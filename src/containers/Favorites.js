48a67f2c517454a91fbe989cc8e99fb48bbb77e8
FranLpz <franciscoalopezd@gmail.com>
2017-07-07 15:22:41 -0400

    Add react-router - Routes

---
+++ src/containers/Favorites.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+    render() {
+        return (
+            <div>
+                My Favorites
+            </div>
+        );
+    }
+}
+
+export default Favorites;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,7 +7,7 @@ import GifsList from '../components/GifList';
 import GifModal from '../components/GifModal';
 import '../style/App.css'
 
-class App extends React.Component {
+class Home extends React.Component {
   render() {
     return (
       <div>
@@ -40,4 +40,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
